bf0e0d8c0583705339f1fd45817294dc0e6ae6e4
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2022-05-04 07:12:24 +0200

    test(assets): remove __esModule' import test

--- test/assets/assets.spec.js
+++ test/assets/assets.spec.js
@@ -1,20 +1,14 @@
 import * as assets from './index'
 
-console.log('ASSETS:\n')
-console.log(assets)
-
 const { chai: { expect }} = global
 
-describe('[Assets] - file-loader', function() {
-  it('export-default-from as namespace', () => {
-    // check export-default-from
-    expect(assets.__esModule).to.be.equal(true) // eslint-disable-line import/namespace
+describe('[Assets] - file-loader combined with export-default-from', function() {
+  it('imported namespace contains asset keys', () => {
     // check import - import-namespace combined with export-default-from
     expect(assets).to.contain.keys('stackr23_logo')
   })
 
-  it('output contains \'[path][name]\'', () => {
-    // check path prefix
+  it('asset key contains \'/[path]/[name].[ext]\'', () => { // check path prefix
     expect(assets.stackr23_logo).to.contain('/assets/stackr23_logo.jpg')
   })
 })
